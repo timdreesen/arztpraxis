d309381d75c50adefa843caad9f8f4fb90bf9cdb
timdreesen <tim.dreesen@gmail.com>
2025-10-26 01:53:22 -0400

    some more optimization

--- src/components/Eingang.tsx
+++ src/components/Eingang.tsx
@@ -3,16 +3,13 @@ import praxisEingang from "@/app/praxis-eingang.jpg";
 
 export default function Eingang() {
   return (
-    <div className="relative w-full max-w-xl mx-auto overflow-hidden rounded-lg shadow-2xl aspect-4/3 sm:my-auto">
+    <div className="relative w-full max-w-xl mx-auto aspect-4/3 overflow-hidden rounded-lg shadow-2xl">
       <Image
         src={praxisEingang}
         alt="der Eingang zur Praxis"
-        width={750} // optimal source width for desktop
-        height={563} // matches 4:3 aspect ratio
-        style={{ objectFit: "cover", width: "100%", height: "100%" }}
-        sizes="(max-width: 640px) 100vw, 50vw"
-        className="rounded-lg shadow-2xl"
-        quality={75} // default is 75; you can reduce to 60 for smaller files
+        fill
+        sizes="(max-width: 640px) 372px, 50vw"
+        style={{ objectFit: "cover" }}
       />
     </div>
   );
